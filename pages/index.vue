fb2026c661d165ecb5019ae8cc7c7ebe8f775c81
haemj <anya.borisowa2017@gmail.com>
2025-06-16 16:51:38 +0300

    anya sdelala po krasote

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,23 @@
   import BlockTwo from "~/blocks/mp/BlockTwo.vue";
   import BlockFour from "~/blocks/mp/BlockFour.vue";
   import BlockTen from "~/blocks/mp/BlockTen.vue";
+  import BlockOne from "~/blocks/mp/BlockOne.vue";
+  import BlockFive from "~/blocks/ann/BlockFive.vue";
+  import BlockSix from "~/blocks/ann/BlockSix.vue";
+  import BlockSeven from "~/blocks/ann/BlockSeven.vue";
+  import BlockNine from "~/blocks/ann/BlockNine.vue";
 </script>
 
 <template>
+    <block-one/>
     <block-two/>
     <block-third/>
     <block-four/>
+    <block-five/>
+    <block-six/>
+    <block-seven/>
     <block-eighth/>
+    <block-nine/>
     <block-ten/>
 </template>
 
