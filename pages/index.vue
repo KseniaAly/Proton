d6d65ad38e0d4b04a2ebd3ec3ffbbb5802ad2ddb
blo0dymari <mariapechilina3000@mail.ru>
2025-06-16 13:53:34 +0300

    pechilina_m

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,18 @@
   import BlockThird from "~/blocks/ksy/BlockThird.vue";
   import BlockEighth from "~/blocks/ksy/BlockEighth.vue";
   import BlockEleventh from "~/blocks/ksy/BlockEleventh.vue";
+  import blockTwo from "~/blocks/mp/blockTwo.vue"
+  import blockFour from "~/blocks/mp/blockFour.vue"
+  import blockTen from "~/blocks/mp/blockTen.vue"
 </script>
 
 <template>
   <div class="page">
+    <block-two></block-two>
     <block-third/>
+    <block-four></block-four>
     <block-eighth/>
+    <block-ten></block-ten>
     <block-eleventh/>
   </div>
 </template>
